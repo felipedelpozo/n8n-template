351063099d0e3fe13a75c5afcf7544a10f58d8ad
Felipe del Pozo <felipedelpozo@dtoo.es>
2024-12-16 11:12:11 +0100

    import workflow test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-n8n import:workflow --separate --input=/home/node/.n8n/nodes/workflows/
-n8n update:workflow --all --active=true
-n8n import:credentials --separate --input=/home/node/.n8n/nodes/credentials
-
 if [ "$#" -gt 0 ]; then
   # Got started with arguments
   exec n8n "$@"
